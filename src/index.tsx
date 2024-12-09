4bec45d19f79e3d12978d9bfe38ff090bda4f917
UdhayakumarEcyber <udhaya@ecyber.com>
2024-12-09 12:15:35 +0530

    Media query added

--- src/index.tsx
+++ src/index.tsx
@@ -8,26 +8,14 @@ import './styles.scss';
 interface IWidgetProps {
     uxpContext?: IContextProvider,
     instanceId?: string
-}
- 
+} 
 
-const Air_quality_bannerWidget: React.FunctionComponent<IWidgetProps> = (props) => { 
-    const data = {
-        AQI: 9,
-        AQIStatus: "Good",
-        temperature: 24,
-        humidity: 57,
-        PM25: 2,
-        CO2: 474,
-        TVOC: 68,
-      };
-
+const Air_quality_bannerWidget: React.FunctionComponent<IWidgetProps> = (props) => {  
 
     return (
-        <WidgetWrapper className="widget-container"> 
- 
+        <WidgetWrapper className="widget-container">  
             
-               <div className="widget-cont">  
+                 <div className="widget-cont">  
                     <div className="details">
 
                         <div className="detail-item detail-item1">
@@ -78,167 +66,7 @@ const Air_quality_bannerWidget: React.FunctionComponent<IWidgetProps> = (props) 
                             <span className="tagline">The Heart of Bangkok</span>
                         </div> 
                     </div>   
-             </div>
-
-             <div className="widget-cont curve-widget-cont">  
-                    <div className="details">
-
-                        <div className="detail-item detail-item1">
-                        
-                            <div className="content"> 
-                                <div className="detail-item-cont"><h4> 9</h4> <em className="value">AQI</em></div>
-                                <span className="status">Good</span>
-                            </div>
-                            <label className="label">US Air Quality Index</label>
-                        </div>
-
-                        <div className="detail-item detail-item2">
-                            
-                        
-                            <div className="content">
-                                <div className="detail-item-cont"><h4>24</h4> <em className="value">°C</em></div>
-                                <span className="status">Temperature</span>
-                            </div>
-
-                            <div className="content"> 
-                                <div className="detail-item-cont"><h4>57</h4><em className="value">%</em></div>
-                                <span className="status">Humidity </span>
-                            </div>
-                            <label className="label">Thermal Comfort</label>
-                        </div>
-
-                        <div className="detail-item detail-item3"> 
-                        
-                            <div className="content"> 
-                                <div className="detail-item-cont"><h4>2</h4><em className="value">µg/m³</em></div>
-                                <span className="status">PM 2.5</span>
-                            </div>
-
-                            <div className="content"> 
-                                <div className="detail-item-cont"><h4>474</h4> <em className="value">ppm</em></div>
-                                <span className="status">Carbon Dioxide</span>
-                            </div>
-
-                            <div className="content"> 
-                                <div className="detail-item-cont"><h4>68</h4><em className="value">µg/m³</em></div>
-                                <span className="status">TVOC</span>
-                            </div>
-                            <label className="label">Air Quality</label>
-                        </div>
-    
-                        <div className="logo-cont">
-                            <span className="brand">One Bangkok</span>
-                            <span className="tagline">The Heart of Bangkok</span>
-                        </div> 
-                    </div>   
-             </div>
-
-             <div className="widget-cont dark-widget-cont">  
-                    <div className="details">
-
-                        <div className="detail-item detail-item1">
-                        
-                            <div className="content"> 
-                                <div className="detail-item-cont"><h4> 9</h4> <em className="value">AQI</em></div>
-                                <span className="status">Good</span>
-                            </div>
-                            <label className="label">US Air Quality Index</label>
-                        </div>
-
-                        <div className="detail-item detail-item2">
-                            
-                        
-                            <div className="content">
-                                <div className="detail-item-cont"><h4>24</h4> <em className="value">°C</em></div>
-                                <span className="status">Temperature</span>
-                            </div>
-
-                            <div className="content"> 
-                                <div className="detail-item-cont"><h4>57</h4><em className="value">%</em></div>
-                                <span className="status">Humidity </span>
-                            </div>
-                            <label className="label">Thermal Comfort</label>
-                        </div>
-
-                        <div className="detail-item detail-item3"> 
-                        
-                            <div className="content"> 
-                                <div className="detail-item-cont"><h4>2</h4><em className="value">µg/m³</em></div>
-                                <span className="status">PM 2.5</span>
-                            </div>
-
-                            <div className="content"> 
-                                <div className="detail-item-cont"><h4>474</h4> <em className="value">ppm</em></div>
-                                <span className="status">Carbon Dioxide</span>
-                            </div>
-
-                            <div className="content"> 
-                                <div className="detail-item-cont"><h4>68</h4><em className="value">µg/m³</em></div>
-                                <span className="status">TVOC</span>
-                            </div>
-                            <label className="label">Air Quality</label>
-                        </div>
-    
-                        <div className="logo-cont">
-                            <span className="brand">One Bangkok</span>
-                            <span className="tagline">The Heart of Bangkok</span>
-                        </div> 
-                    </div>   
-             </div>
-
-
-             <div className="widget-cont dark-widget-cont curve-widget-cont">  
-                    <div className="details">
-
-                        <div className="detail-item detail-item1">
-                        
-                            <div className="content"> 
-                                <div className="detail-item-cont"><h4> 9</h4> <em className="value">AQI</em></div>
-                                <span className="status">Good</span>
-                            </div>
-                            <label className="label">US Air Quality Index</label>
-                        </div>
-
-                        <div className="detail-item detail-item2">
-                            
-                        
-                            <div className="content">
-                                <div className="detail-item-cont"><h4>24</h4> <em className="value">°C</em></div>
-                                <span className="status">Temperature</span>
-                            </div>
-
-                            <div className="content"> 
-                                <div className="detail-item-cont"><h4>57</h4><em className="value">%</em></div>
-                                <span className="status">Humidity </span>
-                            </div>
-                            <label className="label">Thermal Comfort</label>
-                        </div>
-
-                        <div className="detail-item detail-item3"> 
-                        
-                            <div className="content"> 
-                                <div className="detail-item-cont"><h4>2</h4><em className="value">µg/m³</em></div>
-                                <span className="status">PM 2.5</span>
-                            </div>
-
-                            <div className="content"> 
-                                <div className="detail-item-cont"><h4>474</h4> <em className="value">ppm</em></div>
-                                <span className="status">Carbon Dioxide</span>
-                            </div>
-
-                            <div className="content"> 
-                                <div className="detail-item-cont"><h4>68</h4><em className="value">µg/m³</em></div>
-                                <span className="status">TVOC</span>
-                            </div>
-                            <label className="label">Air Quality</label>
-                        </div>
-    
-                        <div className="logo-cont">
-                            <span className="brand">One Bangkok</span>
-                            <span className="tagline">The Heart of Bangkok</span>
-                        </div> 
-                    </div>   
-             </div>
+             </div>   
         </WidgetWrapper>
     )
 };
@@ -251,10 +79,12 @@ registerWidget({
     widget: Air_quality_bannerWidget,
     configs: {
         layout: {
-            // w: 12,
+            // w: 30,
             // h: 12,
             // minH: 12,
             // minW: 12
+            //   maxH: 12,
+            //    maxW: 30
         }
     }
 }); 
